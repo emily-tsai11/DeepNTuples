8efa077235cbacee4004040753339c76534c3f86
David Seith <david.seith@cern.ch>
2019-08-19 12:31:40 +0200

    crab

--- DeepNtuplizer/production/crab.py
+++ DeepNtuplizer/production/crab.py
@@ -1,9 +1,21 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 from WMCore.Configuration import Configuration
+from CRABAPI.RawCommand import crabCommand
 config = Configuration()
 
+import sys
+ds = sys.argv[1]
+rn = ds.split('/')[1]
+
+import os
+
+while os.path.isdir('crab_projects/crab_'+rn):
+    rn += '_0'
+
+
+
 config.section_("General")
-config.General.requestName = 'DeepNtuples_ttbar'
+config.General.requestName = rn
 # config.General.requestName = 'DeepNtuples_qcd'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
@@ -16,13 +28,14 @@ config.JobType.inputFiles = ['QGL_cmssw8020_v2.db']
 
 config.section_("Data")
 # config.Data.inputDataset = '/QCD_Pt-15To7000_TuneCP5_Flat_14TeV-pythia8/PhaseIIMTDTDRAutumn18MiniAOD-PU200_103X_upgrade2023_realistic_v2-v1/MINIAODSIM'
-config.Data.inputDataset = '/TTbar_14TeV_TuneCP5_Pythia8/PhaseIIMTDTDRAutumn18MiniAOD-PU200_103X_upgrade2023_realistic_v2-v1/MINIAODSIM'
+# config.Data.inputDataset = '/TTbar_14TeV_TuneCP5_Pythia8/PhaseIIMTDTDRAutumn18MiniAOD-PU200_103X_upgrade2023_realistic_v2-v1/MINIAODSIM'
+config.Data.inputDataset = ds
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_tutorial_May2015_MC_analysis'
+config.Data.outputDatasetTag = 'DeepNtuples'
 
 config.section_("Site")
 config.Site.storageSite = 'T1_DE_KIT_Disk'
@@ -30,3 +43,4 @@ config.Site.storageSite = 'T1_DE_KIT_Disk'
 config.section_("User")
 config.User.voGroup = 'dcms'
 
+crabCommand('submit', config=config)

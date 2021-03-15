bbca918f0a9e1944565a2e5e94ae0885402e6b09
test <max.neukum@kit.edu>
2021-03-15 17:00:46 +0100

    added additional files. jan21 update

---
+++ additional_files/copy_files.sh
@@ -0,0 +1,2 @@
+cp additional_files/TaggingVariable.cc $CMSSW_BASE/src/DataFormats/BTauReco/src/TaggingVariable.cc
+cp additional_files/TaggingVariable.h $CMSSW_BASE/src/DataFormats/BTauReco/interface/TaggingVariable.h
